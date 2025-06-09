5300950dfd7eb5c87f50bf1460952e1dfe3d4127
imrahul16 <rd7409172@gmail.com>
2025-06-09 23:13:10 +0530

    Task 01 TODO app

--- app/src/main/java/com/example/protodo/MainActivity.kt
+++ app/src/main/java/com/example/protodo/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.GeneratedAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var taskAdapter: TaskAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
